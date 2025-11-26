1b18ca7810f16dac0446b82ad8bc1e8b930a68c1
Pramod JM <pramodjm4@gmail.com>
2025-11-26 20:48:45 +0530

    Change the base URL

--- src/utils/Api.services.ts
+++ src/utils/Api.services.ts
@@ -3,9 +3,9 @@ import AxiosInstance from "./AxiosInstance";
 
 
 // const baseUrl = process.env.REACT_APP_BASE_URL as string;
-const baseUrl ='http://localhost:4500'
+// const baseUrl ='http://localhost:4500'
 
-// const baseUrl ='https://book-hotel-delta-two.vercel.app'
+const baseUrl ='https://book-hotel-delta-two.vercel.app'
 export const loginApi = async (payload: {   
  
     email: String,
