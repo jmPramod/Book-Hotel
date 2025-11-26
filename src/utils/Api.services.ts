0edf942d87254b9ffc493167b648f922ef657a11
Pramod J M <107529743+jmPramod@users.noreply.github.com>
2025-11-26 23:00:41 +0530

    Update base URL for API service
    Comment out the base URL assignment and set it to a production URL.

--- src/utils/Api.services.ts
+++ src/utils/Api.services.ts
@@ -2,10 +2,10 @@
 import AxiosInstance from "./AxiosInstance";
 
 
- const baseUrl = process.env.REACT_APP_BASE_URL as string;
+ // const baseUrl = process.env.REACT_APP_BASE_URL as string;
 //const baseUrl ='http://localhost:4500'
 
-// const baseUrl ='https://book-hotel-delta-two.vercel.app'
+const baseUrl ='https://book-hotel-delta-two.vercel.app'
 export const loginApi = async (payload: {   
  
     email: String,
