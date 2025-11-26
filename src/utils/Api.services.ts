71fdf292ff73aaf50ddc5cd4f0cdd43ccbcb7859
Pramod JM <pramodjm4@gmail.com>
2025-11-26 17:58:27 +0530

    update base url

--- src/utils/Api.services.ts
+++ src/utils/Api.services.ts
@@ -5,7 +5,7 @@ import AxiosInstance from "./AxiosInstance";
 // const baseUrl = process.env.REACT_APP_BASE_URL as string;
 // const baseUrl ='http://localhost:4500'
 
-const baseUrl ='https://book-hotel-n3ht.vercel.app'
+const baseUrl ='https://book-hotel-delta-two.vercel.app'
 export const loginApi = async (payload: {   
  
     email: String,
